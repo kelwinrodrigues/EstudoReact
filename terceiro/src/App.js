af80ac571fc64853bc3f27d1900431e7496370f3
KELWIN DE SENA RODRIGUES <kelwin.srodrigues@senacsp.edu.br>
2022-05-13 09:44:58 -0300

    modificação do App.js

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,17 +1,15 @@
 function App() {
-  let valor1 = Math.round(Math.random() * 200);
-  let valor2 = Math.round(Math.random() * 200);
+  let styles = {
+    backgroundColor: "#300",
+    color: "#fff",
+    display: "flex",
+    flex: 1,
+    height: "100vh",
+  };
 
   return (
-    <div>
-      <p>A soma entre 50 e 10 é {50 + 10}</p>
-      <p>
-        {" "}
-        A soma entre {valor1} e {valor2} é {valor1 + valor2}
-      </p>
-      <p>
-        O resultado acima é {(valor1 + valor2) % 2 === 0 ? "Par" : "Impar"}{" "}
-      </p>
+    <div style={styles}>
+      <p>Veja o background</p>
     </div>
   );
 }
