5b20ce2ae3d127d152619f315702709ed0e901cb
KELWIN DE SENA RODRIGUES <kelwin.srodrigues@senacsp.edu.br>
2022-05-13 11:52:10 -0300

    Adição de elementos ao objetos

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,12 +1,58 @@
 import { useState } from "react";
 
 function App() {
-  const [n, setN] = useState(1);
+  const [produto, setProduto] = useState([
+    {
+      id: 500,
+      nome: "Mouse",
+      categoria: "Informática",
+      quantidade: 15,
+      preco: 50.0,
+    },
+    {
+      id: 501,
+      nome: "Teclado",
+      categoria: "Informática",
+      quantidade: 20,
+      preco: 150.9,
+    },
+  ]);
 
   return (
     <div>
-      <p>{n}</p>
-      <button onClick={() => setN(n + 1)}>Clique</button>
+      <button
+        onClick={() => {
+          var cod = prompt("Entre com o código");
+          var no = prompt("Entre com o nome do produto");
+          var cat = prompt("Entre com a categoria");
+          var qt = prompt("Entre com a quantidade");
+          var pr = prompt("Entre com o preço");
+
+          setProduto(
+            produto.concat({
+              id: cod,
+              nome: no,
+              categoria: cat,
+              quantidade: qt,
+              preco: pr,
+            })
+          );
+        }}
+      >
+        Clique
+      </button>
+
+      {produto.map((item, index) => (
+        <div key={index}>
+          <h2>{item.nome}</h2>
+          <ul>
+            <li>Código: {item.id}</li>
+            <li>Categoria: {item.categoria}</li>
+            <li>Quantidade: {item.quantidade}</li>
+            <li>Preco: {item.preco}</li>
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
